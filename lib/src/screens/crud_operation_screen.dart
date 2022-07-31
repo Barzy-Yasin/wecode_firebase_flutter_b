461cb77e5a9926dcd8ea31bb80b996dbd7127ef9
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-07-31 14:43:58 +0300

    fixing bugs + connecting with Firestore(upload data onPressed)

--- lib/src/screens/crud_operation_screen.dart
+++ lib/src/screens/crud_operation_screen.dart
@@ -1,14 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore: unused_import
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class CrudOperationsScreen extends StatelessWidget {
-  const CrudOperationsScreen({Key? key}) : super(key: key);
+  CrudOperationsScreen({Key? key}) : super(key: key);
+
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('hello'),
+      appBar: AppBar(),
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('hello'),
+              ElevatedButton(
+                onPressed: () {
+                  addNameToDB();
+                },
+                child: Text('Add Data'),
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  addNameToDB() {
+    _firebaseFirestore.collection('names').add({
+      "first_name": "Baralilo_2",
+    });
   }
 }
