b30702c0d19096c43878f73c7fdd749eb97a057c
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-08-02 10:44:27 +0300

    adding future builder to return all docs inside 'names' collection

--- lib/src/screens/crud_operation_screen.dart
+++ lib/src/screens/crud_operation_screen.dart
@@ -1,9 +1,5 @@
 // ignore_for_file: prefer_const_constructors, no_leading_underscores_for_local_identifiers
 
-// import 'dart:html';
-
-// import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 // ignore: unused_import
 import 'package:firebase_core/firebase_core.dart';
@@ -39,21 +35,63 @@ class _CrudOperationsScreenState extends State<CrudOperationsScreen> {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [ 
-              // rendering read data readSingleDocument() 
+            children: [
+              Column(
+                children: [
+                  Text('read the entire collection once'),
+                  Container(
+                    height: 350,
+                    child: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                      future: getDataOnceUsingFuture(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return Center(child: CircularProgressIndicator());
+                        } else if (snapshot.hasError) {
+                          return Text(snapshot.error.toString());
+                        } else if (snapshot.data == null) {
+                          return Text('snapshottt no data');
+                        }
+                        // return Text(snapshot.data.toString());
+                        // return Text(snapshot.data!.docs.toString()); // returns firestore instances
+                        // return Text(snapshot.data!.docs.length.toString()); // returns number of documents inside the collection
+
+                        // snapshot.data!.docs.map((doc) => print (doc.data()));
+                        // return Text(snapshot.data!.docs.length.toString());
+                        return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              color: Colors.yellow.shade100,
+                              alignment: Alignment.topLeft,
+                              margin: EdgeInsets.symmetric(vertical: 1, horizontal: 20),
+                              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                              child: Text("${index+1}: ${snapshot.data!.docs[index]
+                                  .data()["first_name"]}"),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+
+              // rendering read data readSingleDocument()
+              Text('read one specific document'),
               Container(
                 child: FutureBuilder<DocumentSnapshot>(
                   future: readSingleDocument(),
                   builder: (context, snapshot) {
-                     if (snapshot.connectionState == ConnectionState.waiting) {
-                       return CircularProgressIndicator();
-                     } else if (snapshot.data == null || !snapshot.hasData) {
-                       return Text('snapshot is empty');
-                     } else if (snapshot.hasError) {
-                       return Text(snapshot.error.toString());
-                     }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    } else if (snapshot.data == null || !snapshot.hasData) {
+                      return Text('snapshot is empty');
+                    } else if (snapshot.hasError) {
+                      return Text(snapshot.error.toString());
+                    }
                     //  return Text(snapshot.data!.data().toString());
-                    Map<String, dynamic> name = snapshot.data!.data() as Map<String, dynamic>;
+                    Map<String, dynamic> name =
+                        snapshot.data!.data() as Map<String, dynamic>;
                     return Text(name.values.first);
                   },
                 ),
@@ -78,19 +116,19 @@ class _CrudOperationsScreenState extends State<CrudOperationsScreen> {
               Divider(
                 height: 25,
               ),
-              // add data button 
+              // add data button
               ElevatedButton(
                 onPressed: () async {
                   if (_formKey.currentState!.validate() == true) {
                     debugPrint('form validated');
                     await addNameToDB(name: _nameController.value.text)
                         .then((value) {
-                          debugPrint(value.path);
-                          setState(() {
-                            valueId = value.id;
-                          });
-                          debugPrint('current value.id=  ${value.id}');
-                        });
+                      debugPrint(value.path);
+                      setState(() {
+                        valueId = value.id;
+                      });
+                      debugPrint('current value.id=  ${value.id}');
+                    });
                   } else {
                     debugPrint('form not validated');
                   }
@@ -112,11 +150,17 @@ class _CrudOperationsScreenState extends State<CrudOperationsScreen> {
     return _doc;
   }
 
-  // read data from firestore
+  // read data from firestore, one element
   Future<DocumentSnapshot> readSingleDocument() async {
     DocumentSnapshot _doc =
         await _firebaseFirestore.collection("names").doc(valueId).get();
-  print(_doc.data());
+    print(_doc.data());
     return _doc;
   }
+
+  // returning all the data inside a collection once 
+  Future<QuerySnapshot<Map<String, dynamic>>> getDataOnceUsingFuture() async {
+    return await _firebaseFirestore.collection('names').get();
+  }
+  
 }
