7e171dd0abf56c56f66b8c2ad42f808c300a839d
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-08-07 04:36:53 +0300

    login screen ui + some changes of CrudOperationsScreen

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,57 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          const Text('welcome'),
+          Container(
+            margin: const EdgeInsets.all(15),
+            padding: const EdgeInsets.all(15),
+            child: Form(
+                child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const Text('user name/email'),
+                TextFormField(
+                  controller: _userNameController,
+                ),
+                const SizedBox(height: 25),
+                const Text('password'),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 25),
+                // ignore: avoid_unnecessary_containers
+                Container(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        print('jjjjj');
+                      },
+                      child: const Text(
+                        "login",
+                      )),
+                )
+              ],
+            )),
+          )
+        ],
+      ),
+    );
+  }
+}
